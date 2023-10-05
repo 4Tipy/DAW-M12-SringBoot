972ea92b168b755ccbbb14f6ccd26cdbc1cb1561
leonardo <leonardo@hotmail.com>
2023-10-05 16:19:13 +0200

    avances en el jpa spring

--- primerProyectoTIC/src/main/java/proyecto1/controlador/controlador.java
+++ primerProyectoTIC/src/main/java/proyecto1/controlador/controlador.java
@@ -2,6 +2,7 @@ package proyecto1.controlador;
 
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import proyecto1.bean.Libro;
 import proyecto1.bean.Usuario;
 import proyecto1.repository.BaseDatos;
+import proyecto1.repository.BaseDatos2;
+import proyecto1.repository.BaseDatos3;
 
 import org.springframework.ui.Model; 
 
@@ -19,8 +22,12 @@ import org.springframework.ui.Model;
 @RequestMapping("")
 public class controlador {
 	
-	BaseDatos bd = new BaseDatos();
-	Usuario usuario;
+//	Usuario usuario;
+//	BaseDatos2 bd = new BaseDatos2();
+	
+	@Autowired
+	BaseDatos3 bd;
+private Object usuario;
 	
 	@GetMapping("/")
 	public String iniciar(Model model) {
@@ -33,7 +40,7 @@ public class controlador {
 	@PostMapping("/")
 	public String login(Usuario usuario, Model model) {
 		if (usuario.getNombre().equals("leo") && usuario.getPassword().equals("123")) {
-			ArrayList<Libro> libros = bd.getLibros();
+			ArrayList<Libro> libros = (ArrayList<Libro>)bd.findAll();
 			model.addAttribute("usuario", usuario);
 			this.usuario = usuario;
 			model.addAttribute("libros", libros);
@@ -44,48 +51,52 @@ public class controlador {
 	
 	@PostMapping("/insertar")
 	public String insertar (Libro libro, Model model) {
-		bd.inserta(libro);
-		ArrayList<Libro> libros = bd.getLibros();
+//		bd.inserta(libro);
+		bd.save(libro);
+		ArrayList<Libro> libros = (ArrayList<Libro>)bd.findAll();
 		model.addAttribute("usuario", this.usuario);
 		model.addAttribute("libros", libros);
-		return "consulta";
-	}
-	
-	@GetMapping("/borrado/{id}")
-	public String borrar(@PathVariable int id, Model model) {
-		bd.borrar(id);
-		ArrayList<Libro> libros = bd.getLibros();
-		model.addAttribute("libros", libros);
-		model.addAttribute("usuario", this.usuario);
 		model.addAttribute("boton", "Insertar Libro");
 		model.addAttribute("action", "/insertar");
-		return "consulta";
-	}
-	
-	@GetMapping("/modificar/{id}")
-	public String modificar(@PathVariable int id, Model model) {
-	    Libro libro = bd.getLibro(id); // Utiliza el nuevo método getLibro
-	    ArrayList<Libro> libros = bd.getLibros();
-	    model.addAttribute("libros", libros);
-	    model.addAttribute("libro", libro);
-	    model.addAttribute("usuario", this.usuario);
-	    model.addAttribute("boton", "Actualiza libro");
-	    model.addAttribute("action", "/modificar");
-	    return "consulta";
-	}
-
-	
-	@PostMapping("/modificar")
-	public String modificar2(Libro libro, Model model) {
-		bd.modifica(libro);
-		ArrayList<Libro> libros = bd.getLibros();
-		model.addAttribute("usuario", this.usuario);
-		model.addAttribute("libros", libros);
 		model.addAttribute("libro", null);
-		model.addAttribute("boton", "Insertar libro");
-		model.addAttribute("action", "/insertar");
 		return "consulta";
 	}
+	
+//	@GetMapping("/borrado/{id}")
+//	public String borrar(@PathVariable int id, Model model) {
+//		bd.borrar(id);
+//		ArrayList<Libro> libros = bd.getLibros();
+//		model.addAttribute("libros", libros);
+//		model.addAttribute("usuario", this.usuario);
+//		model.addAttribute("boton", "Insertar Libro");
+//		model.addAttribute("action", "/insertar");
+//		return "consulta";
+//	}
+//	
+//	@GetMapping("/modificar/{id}")
+//	public String modificar(@PathVariable int id, Model model) {
+//	    Libro libro = bd.getLibro(id); // Utiliza el nuevo método getLibro
+//	    ArrayList<Libro> libros = bd.getLibros();
+//	    model.addAttribute("libros", libros);
+//	    model.addAttribute("libro", libro);
+//	    model.addAttribute("usuario", this.usuario);
+//	    model.addAttribute("boton", "Actualiza libro");
+//	    model.addAttribute("action", "/modificar");
+//	    return "consulta";
+//	}
+//
+//	
+//	@PostMapping("/modificar")
+//	public String modificar2(Libro libro, Model model) {
+//		bd.modifica(libro);
+//		ArrayList<Libro> libros = bd.getLibros();
+//		model.addAttribute("usuario", this.usuario);
+//		model.addAttribute("libros", libros);
+//		model.addAttribute("libro", null);
+//		model.addAttribute("boton", "Insertar libro");
+//		model.addAttribute("action", "/insertar");
+//		return "consulta";
+//	}
 	
 	
 	
